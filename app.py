361be4101541b2c9de151ca352685d484fe41c73
omsingh-coder <omsinghvmc@gmail.com>
2025-11-27 13:32:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,12 @@ import os
 from flask import Flask, request, send_file
 from twilio.rest import Client
 
-app = Flask(__name__)
+app = Flask(name)
 
-# Twilio credentials from environment variables
-account_sid = 
-os.environ.get("AC2d1ff15cbc93ae35758549ea64c9a146")
-auth_token =
-os.environ.get("28e781f694ac5e9c0bb5d2c5a98a5026")
-twilio_number =
-os.environ.get("+18287959778")
+# Twilio credentials from Railway environment variables
+account_sid = os.environ.get("AC2d1ff15cbc93ae35758549ea64c9a146")
+auth_token = os.environ.get("28e781f694ac5e9c0bb5d2c5a98a5026")
+twilio_number = os.environ.get(" +18287959778")
 my_phone = os.environ.get("+919142574197")
 
 client = Client(account_sid, auth_token)
@@ -24,6 +21,7 @@ def home():
 def send_reply():
     data = request.json
     message_text = data.get('message')
+
     if not message_text:
         return {"error": "No message provided"}, 400
 
@@ -43,6 +41,6 @@ def send_reply():
 
     return {"success": True}, 200
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+if name == "main":
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
